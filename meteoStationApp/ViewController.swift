692b5b4fcf9f4b3b7463de8102c55cd5e7c9d844
Walid Sassi <walid@imac-de-walid.local>
2017-01-22 14:34:05 +0100

    Initial Commit

---
+++ meteoStationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meteoStationApp
+//
+//  Created by Walid Sassi on 22/01/2017.
+//  Copyright © 2017 Walid Sassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
